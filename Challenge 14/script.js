e47c71db6b20f8a041153fca6db30f5f719e36be
Zarrouk Meriem <zarroukmeriem19@gmail.com>
2024-10-31 09:39:57 +0100

    warmup2_challenge14

---
+++ Challenge 14/script.js
@@ -0,0 +1,11 @@
+//////////////////////////////////////////////
+//////// 14)
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+console.log(factorial(10));
+// 3628800
